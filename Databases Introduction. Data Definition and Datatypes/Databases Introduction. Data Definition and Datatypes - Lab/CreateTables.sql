63411830a7397c6444b52b8ea69ceacc9f797d63
Antoan Metodiev <Boksnem@abv.bg>
2024-08-17 23:56:21 +0300

    add solution

---
+++ Databases Introduction. Data Definition and Datatypes/Databases Introduction. Data Definition and Datatypes - Lab/CreateTables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE minions(
+    id INT auto_increment primary key NOT NULL,
+    name VARCHAR(50),
+    age INT
+);
+
+CREATE TABLE towns(
+   town_id INT auto_increment primary key NOT NULL,
+   name VARCHAR(50)
+);
+
+
