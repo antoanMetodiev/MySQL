6ae11dbe29d2d058cf9303f2d6a8d5c39ad755e9
Antoan Metodiev <Boksnem@abv.bg>
2024-08-30 22:24:04 +0300

    add solution

---
+++ Table Relations/MountainsAndPeaks.sql
@@ -0,0 +1,13 @@
+CREATE TABLE mountains(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100)
+);
+
+
+CREATE TABLE peaks(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    mountain_id INT,
+
+    CONSTRAINT fk_mountain_id FOREIGN KEY(mountain_id) REFERENCES mountains(id)
+);
