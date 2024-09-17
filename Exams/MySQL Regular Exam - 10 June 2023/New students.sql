904d46df448649864f7598e17408b0108c46d0f0
Antoan Metodiev <Boksnem@abv.bg>
2024-09-17 23:15:51 +0300

    add solution

---
+++ Exams/MySQL Regular Exam - 10 June 2023/New students.sql
@@ -0,0 +1,5 @@
+SELECT CONCAT(first_name, ' ', last_name) AS full_name,
+		SUBSTRING(email, 2, 10) AS 'username', REVERSE(phone)
+        AS 'password' FROM students
+        WHERE id NOT IN (SELECT student_id FROM students_courses)
+        ORDER BY password DESC;
