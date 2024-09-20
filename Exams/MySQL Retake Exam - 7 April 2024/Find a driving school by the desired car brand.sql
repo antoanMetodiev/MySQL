33bcbbd20139bee60cda8fd7529ed4da643cfff8
Antoan Metodiev <Boksnem@abv.bg>
2024-09-20 20:29:42 +0300

    add solution

---
+++ Exams/MySQL Retake Exam - 7 April 2024/Find a driving school by the desired car brand.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE udp_find_school_by_car(brand VARCHAR(20))
+BEGIN
+	SELECT ds.name, ds.average_lesson_price FROM driving_schools ds
+	JOIN cars c ON c.id = ds.car_id
+	WHERE c.brand = brand
+	ORDER BY ds.average_lesson_price DESC;
+END
