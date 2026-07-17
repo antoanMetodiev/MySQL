100d50d625fea3c245b51ed76bb13cd9817ef730
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2026-07-17 16:32:05 +0300

    add another solution

--- Lessons/Basic CRUD/Basic CRUD - Lab/SelectEmployeesWithFilter.sql
+++ Lessons/Basic CRUD/Basic CRUD - Lab/SelectEmployeesWithFilter.sql
@@ -1,7 +1,3 @@
-SELECT
-id,
-CONCAT_WS(' ', first_name, last_name) AS 'fullname',
-job_title,
-salary from employees
+SELECT id, concat(first_name, ' ', last_name) AS full_name, job_title, salary FROM employees
 WHERE salary > 1000.00
 ORDER BY id;
